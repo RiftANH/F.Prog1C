01c87647dbe8a1993791048c0b8bade8ad4f67d8
RiftANH <143563089+RiftANH@users.noreply.github.com>
2023-09-13 16:54:03 -0700

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/main/java/com/mycompany/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_identificadores;
+
+/**
+ *
+ * @author Eclip
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        int $nombre;
+        int _nombre;
+        int nombre1;
+        int nombre;
+        int Nombre;
+        int nombre_dato;
+        //int nombre; la variable ya existe previamente
+        
+        //int public; no podemos usar public, es palabra reservada (keyword)
+        int Public;
+        double asdfghjklp;
+    }
+}

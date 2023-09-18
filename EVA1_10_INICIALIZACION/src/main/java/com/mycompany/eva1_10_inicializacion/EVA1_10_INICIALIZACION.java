e24601698302c9851e528242488c8407ec19bfdc
RiftANH <143563089+RiftANH@users.noreply.github.com>
2023-09-18 16:52:19 -0700

    Add files via upload

---
+++ EVA1_10_INICIALIZACION/src/main/java/com/mycompany/eva1_10_inicializacion/EVA1_10_INICIALIZACION.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_inicializacion;
+
+/**
+ *
+ * @author Eclip
+ */
+public class EVA1_10_INICIALIZACION {
+
+    public static void main(String[] args) {
+        int valor=0; //INICIALIZANDO LA VARIABLE
+        System.out.println(valor);
+        
+        int precio; //Declarar
+        precio=100; //Inicializar
+        
+        double salario = 0 , precioMayoreo = 100 , iva = 10.5 , subtotal = 15 , total = 100.0; //tenemos 5 variables
+        
+        //CONSTANTES
+        //final --. TE PERMITE ASIGNAR UN VALOR SOLO UNA VEZ
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+        
+        /*final double miValorPi = 3.1416;
+        final String ESCUELA="Instituto Tecnologico de Chihuahua II";
+        final int CALIFA_PASAR = 70;*/
+        
+        /*
+            Varias lines de commentarios
+            cuando quieren tirar mucho rollo
+            esta es la mejor opcion.
+        */
+        int x;//COMENTARIO DE UNA SOLA LINEA, TODO EN LA LINEA ES UN COMENTARIO.
+                
+    }
+}
